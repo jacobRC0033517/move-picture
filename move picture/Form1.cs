5406016dbd323910cf52cf110de28f8f18b6bb60
jacob thornton <Jet2002Jake@gmail.com>
2018-09-11 11:34:52 +0100

    image can move to the bottom left

--- move picture/Form1.cs
+++ move picture/Form1.cs
@@ -46,5 +46,10 @@ namespace move_picture
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y - 5);
         }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y + 5);
+        }
     }
 }
