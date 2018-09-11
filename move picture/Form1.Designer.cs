7f524b71371f465beb6ce049b755cab2363c9d1b
jacob thornton <Jet2002Jake@gmail.com>
2018-09-11 10:42:35 +0100

    picture can move up

--- move picture/Form1.Designer.cs
+++ move picture/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,19 +46,30 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(577, 58);
+            this.button1.Location = new System.Drawing.Point(649, 71);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(62, 45);
             this.button1.TabIndex = 1;
-            this.button1.Text = "move";
+            this.button1.Text = "right";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(607, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "up";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
@@ -71,6 +83,7 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
